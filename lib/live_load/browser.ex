bef4aefaec4eab4bdf948af5be1e07469795e2d0
Coby Benveniste <coby.benveniste@gmail.com>
2025-12-25 20:16:09 +0200

    Set up browser struct and supervisor

--- lib/live_load/browser.ex
+++ lib/live_load/browser.ex
@@ -1,3 +1,31 @@
 defmodule LiveLoad.Browser do
-  @moduledoc false
+  @moduledoc """
+  TODO: Let's document the LiveLoad.Browser module!
+  """
+
+  alias __MODULE__
+  alias LiveLoad.Browser.Connection
+
+  @type t() :: %__MODULE__{
+          connection_mod: Connection.t(),
+          supervisor_pid: pid()
+        }
+
+  defstruct [:connection_mod, :supervisor_pid]
+
+  @spec start_link(connection_mod :: Connection.t(), opts :: Keyword.t()) :: {:ok, t()} | {:error, term()}
+  def start_link(connection_mod, opts \\ []) when is_atom(connection_mod) do
+    browser = %Browser{connection_mod: connection_mod}
+
+    case Browser.Supervisor.start_link(connection_mod, opts) do
+      {:ok, pid} when is_pid(pid) -> {:ok, %{browser | supervisor_pid: pid}}
+      {:error, {:already_started, pid}} when is_pid(pid) -> {:ok, %{browser | supervisor_pid: pid}}
+      {:error, _reason} = error -> error
+    end
+  end
+
+  @spec stop(browser :: t(), reason :: term(), timeout :: timeout()) :: :ok
+  def stop(%Browser{} = browser, reason \\ :normal, timeout \\ :infinity) do
+    Supervisor.stop(browser.supervisor_pid, reason, timeout)
+  end
 end
--- lib/live_load/browser/supervisor.ex
+++ lib/live_load/browser/supervisor.ex
@@ -1,3 +1,55 @@
 defmodule LiveLoad.Browser.Supervisor do
   @moduledoc false
+  use Supervisor
+
+  def start_link(connection_mod, connection_args \\ []) do
+    Supervisor.start_link(__MODULE__, {connection_mod, connection_args})
+  end
+
+  def child_spec(connection_mod, connection_args \\ [], supervisor_opts \\ []) do
+    default = %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, [connection_mod, connection_args]},
+      type: :supervisor
+    }
+
+    Supervisor.child_spec(default, supervisor_opts)
+  end
+
+  @impl Supervisor
+  def init({connection_mod, connection_args}) do
+    children = [
+      Supervisor.child_spec({connection_mod, connection_args}, id: :connection)
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+
+  def connection_pid!(supervisor) do
+    case find_child(supervisor, :connection) do
+      connection when is_pid(connection) ->
+        connection
+
+      nil ->
+        raise RuntimeError, """
+        The connection could not be found under the given supervisor!
+        This is a critical issue and should be reported to the maintainers.
+
+        Please file issues at: https://github.com/probably-not/live-load/issues.
+        """
+    end
+  end
+
+  defp find_child(supervisor, child_id) do
+    supervisor
+    |> Supervisor.which_children()
+    |> Enum.find(fn
+      {^child_id, pid, _type, _modules} when is_pid(pid) -> true
+      _ -> false
+    end)
+    |> then(fn
+      {^child_id, pid, _type, _modules} when is_pid(pid) -> pid
+      _other -> nil
+    end)
+  end
 end
