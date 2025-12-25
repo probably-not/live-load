69af95fe9299605dd7810d1272afe7588e086cae
Coby Benveniste <coby.benveniste@gmail.com>
2025-12-25 19:26:55 +0200

    Move git_hooks config to dev.exs since it is a dev only experience

--- config/dev.exs
+++ config/dev.exs
@@ -1,1 +1,25 @@
 import Config
+
+config :git_hooks,
+  auto_install: true,
+  verbose: true,
+  hooks: [
+    post_checkout: [
+      tasks: [
+        {:mix_task, :"deps.get"},
+        {:mix_task, :"deps.compile"}
+      ]
+    ],
+    pre_commit: [
+      tasks: [
+        {:mix_task, :format, ["--check-formatted"]},
+        {:mix_task, :credo}
+      ]
+    ],
+    pre_push: [
+      tasks: [
+        {:mix_task, :test},
+        {:mix_task, :"deps.unlock", ["--check-unused"]}
+      ]
+    ]
+  ]
