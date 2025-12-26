1be9a27e71c13538c2a13f76d2275d51651ad47e
Coby Benveniste <coby.benveniste@gmail.com>
2025-12-26 13:11:14 +0200

    Extract the supervisor stuff

--- lib/live_load/browser/connection/playwright.ex
+++ lib/live_load/browser/connection/playwright.ex
@@ -1,12 +1,9 @@
 defmodule LiveLoad.Browser.Connection.Playwright do
   @moduledoc false
 
-  use Supervisor
   use LiveLoad.Browser.Connection
 
-  alias __MODULE__
   alias LiveLoad.Browser
-  alias LiveLoad.Browser.Connection
   alias LiveLoad.Browser.Context
 
   @type option() ::
@@ -15,7 +12,10 @@ defmodule LiveLoad.Browser.Connection.Playwright do
           | {:playwright_version, Version.version()}
           | {:startup_timeout, pos_integer()}
 
-  @impl Connection
+  @impl true
+  defdelegate start_link(opts), to: LiveLoad.Browser.Connection.Playwright.Supervisor
+
+  @impl true
   def new_context(%Browser{} = browser) do
     playwright_browser = browser.private.playwright_connection_browser
 
@@ -25,46 +25,9 @@ defmodule LiveLoad.Browser.Connection.Playwright do
     end
   end
 
-  @impl Connection
-  def start_link(opts) do
-    validations = [
-      :playwright_cli_path,
-      name: __MODULE__,
-      playwright_version: playwright_version_from_env(),
-      startup_timeout: 1000
-    ]
-
-    opts = Keyword.validate!(opts, validations)
-
-    {name, opts} = Keyword.pop!(opts, :name)
-    {timeout, opts} = Keyword.pop!(opts, :startup_timeout)
-    {playwright_version, opts} = Keyword.pop!(opts, :playwright_version)
-
-    {playwright_cli_path, _opts} =
-      Keyword.pop_lazy(opts, :playwright_cli_path, fn ->
-        Playwright.Decompressor.extract!(playwright_version)
-      end)
-
-    Supervisor.start_link(__MODULE__, {playwright_cli_path, timeout}, name: name)
-  end
-
-  @impl Connection
+  @impl true
   def after_start(%Browser{} = browser) do
     {:ok, playwright_browser} = PlaywrightEx.launch_browser(:chromium, timeout: 10_000)
     Browser.put_private(browser, :playwright_connection_browser, playwright_browser)
-  end
-
-  @impl Supervisor
-  def init({playwright_cli_path, timeout}) do
-    children = [
-      {PlaywrightEx.Supervisor, [executable: playwright_cli_path, timeout: timeout]}
-    ]
-
-    Supervisor.init(children, strategy: :one_for_one)
-  end
-
-  @default_playwright_version "1.57.0"
-  defp playwright_version_from_env do
-    Application.get_env(:live_load, :playwright_version, @default_playwright_version)
   end
 end
