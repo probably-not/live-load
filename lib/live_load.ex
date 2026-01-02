70aea02edab129b3c955322c91259f68088d991f
Coby Benveniste <coby.benveniste@gmail.com>
2026-01-02 13:28:33 +0200

    Build config options and allow scenario to properly error out on config callback

--- lib/live_load.ex
+++ lib/live_load.ex
@@ -11,34 +11,76 @@ defmodule LiveLoad do
 
   @spec run(opts :: opts()) :: {:ok, results()} | {:error, term()}
   def run(opts) do
-    Application.stop(:amoc)
-    Application.ensure_all_started(:amoc)
     scenarios = discover_scenarios(opts)
     # TODO: Start up FLAME Pool (or Pools, if we have regionality involved?)
-    Enum.each(scenarios, &run_scenario/1)
-    {:ok, nil}
+    Enum.map(scenarios, &{&1, run_scenario(&1, build_options(opts))})
   end
 
   defp discover_scenarios(_opts) do
     [LiveLoad.Scenario.Example]
   end
 
+  # TODO: Amoc takes full control of the node - that's why we need to do all of this
+  # start and stop stuff every time. Should we use the peer module to initialize things under their own nodes?
   # TODO: How are we running scenarios?
   # - Raise FLAME nodes with Trackable that stays alive until we are done running the test
   # - Collect all nodes from FLAME pools.
   # - On the controller node, run `:amoc_cluster.connect_nodes(flame_node_list)`
   # - On the controller node, run `:amoc_dist.do(scenario_mod, user_count, settings)`
-  defp run_scenario(scenario) do
+  defp run_scenario(scenario, opts) do
     Application.stop(:amoc)
     Application.ensure_all_started(:amoc)
-    :amoc.do(scenario, 10, runner_pid: self())
+
+    case :amoc.do(scenario, 10, opts) do
+      :ok ->
+        wait(scenario)
+
+      # `:amoc_dist.do` returns a tuple of `{:ok, any()}`,
+      # so we handle it in the same way as a normal `:ok` here.
+      {:ok, _} ->
+        wait(scenario)
+
+      {:error, reason} ->
+        {:error, reason}
+
+      # This is a catch for the current version of AMoC.
+      # It does not extract the error value properly.
+      # When the new release happens, we can remove this and just rely on the above match.
+      {{:error, reason}, _} ->
+        {:error, reason}
+    end
   after
+    Application.stop(:amoc)
+  end
+
+  defp wait(scenario) do
     # TODO: Wrap this up and wait for all of the nodes in the test to complete
     receive do
-      {:scenario_timeout, _scenario, _completed_node} ->
+      {:scenario_timeout, ^scenario, _completed_node} ->
         :ok
     end
+  end
 
-    Application.stop(:amoc)
+  defp build_options(nil) do
+    [runner_pid: self()]
+  end
+
+  defp build_options(opts) do
+    {runner_opts, scenario_config_opts} =
+      Enum.reduce(
+        [{:timeout, :scenario_timeout}, {:heartbeat, :heartbeat_timeout_seconds}],
+        {[runner_pid: self()], opts},
+        fn {key, replacement}, {runner_opts, scenario_config_opts} ->
+          {opt, rest} = Keyword.pop(scenario_config_opts, key, :error)
+
+          if opt == :error do
+            {runner_opts, rest}
+          else
+            {Keyword.put(runner_opts, replacement, opt), rest}
+          end
+        end
+      )
+
+    Keyword.put(runner_opts, :scenario_config_opts, scenario_config_opts)
   end
 end
--- lib/live_load/scenario/init.ex
+++ lib/live_load/scenario/init.ex
@@ -5,28 +5,31 @@ defmodule LiveLoad.Scenario.Init do
     runner_pid = :amoc_config.get(:runner_pid)
     heartbeat_timeout = :amoc_config.get(:heartbeat_timeout_seconds)
     scenario_timeout = :amoc_config.get(:scenario_timeout)
+    opts = :amoc_config.get(:scenario_config_opts)
 
-    plan = [
-      {:all,
-       fn {:timeout, _count} ->
-         send(runner_pid, {:scenario_timeout, scenario, node()})
-       end}
-    ]
+    with {:ok, scenario_config} <- scenario.config(opts) do
+      plan = [
+        {:all,
+         fn {:timeout, _count} ->
+           send(runner_pid, {:scenario_timeout, scenario, node()})
+         end}
+      ]
 
-    # We manually increase the coordinator timeout by a few seconds.
-    # The heartbeat will run on the heartbeat timeout, and will die off
-    # when the running process completes.
-    coordinator_timeout = heartbeat_timeout + 3
-    :amoc_coordinator.start({scenario, :heartbeat}, plan, coordinator_timeout)
+      # We manually increase the coordinator timeout by a few seconds.
+      # The heartbeat will run on the heartbeat timeout, and will die off
+      # when the running process completes.
+      coordinator_timeout = heartbeat_timeout + 3
+      :amoc_coordinator.start({scenario, :heartbeat}, plan, coordinator_timeout)
 
-    {:ok,
-     %{
-       scenario_config: scenario.config(),
-       __config__: %{
-         runner_pid: runner_pid,
-         heartbeat_timeout: to_timeout(second: heartbeat_timeout),
-         scenario_timeout: scenario_timeout
-       }
-     }}
+      {:ok,
+       %{
+         scenario_config: scenario_config,
+         __config__: %{
+           runner_pid: runner_pid,
+           heartbeat_timeout: to_timeout(second: heartbeat_timeout),
+           scenario_timeout: scenario_timeout
+         }
+       }}
+    end
   end
 end
