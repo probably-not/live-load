640d13fd1934a75d374d993ec388442b49f19f49
Coby Benveniste <coby.benveniste@gmail.com>
2026-03-20 10:03:32 +0200

    Remove no longer needed dialyzer supression now that we have upgraded playwright_ex

--- .dialyzer.ignore-warnings.exs
+++ .dialyzer.ignore-warnings.exs
@@ -1,8 +1,4 @@
 [
   {"lib/mix/tasks/init_playwright.ex", "Callback info about the Mix.Task behaviour is not available."},
-  {"lib/mix/tasks/init_playwright.ex", "Function Mix.shell/0 does not exist."},
-  # TODO: This looks like maybe a spec issue inside PlaywrightEx.
-  # Maybe we can fix it at some point...
-  {"lib/live_load/browser/connection/playwright.ex", "Function after_start/1 has no local return."},
-  {"lib/live_load/browser/connection/playwright.ex", "The function call launch_browser will not succeed."}
+  {"lib/mix/tasks/init_playwright.ex", "Function Mix.shell/0 does not exist."}
 ]
