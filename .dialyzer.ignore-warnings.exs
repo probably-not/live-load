a72743a16437f37889c32e19204fd230e009aa93
Coby Benveniste <coby.benveniste@gmail.com>
2026-03-20 10:01:50 +0200

    Remove no longer needed dialyzer supression now that we delegate to do_scenario and can decide whether we are running distributed or not

--- .dialyzer.ignore-warnings.exs
+++ .dialyzer.ignore-warnings.exs
@@ -4,7 +4,5 @@
   # TODO: This looks like maybe a spec issue inside PlaywrightEx.
   # Maybe we can fix it at some point...
   {"lib/live_load/browser/connection/playwright.ex", "Function after_start/1 has no local return."},
-  {"lib/live_load/browser/connection/playwright.ex", "The function call launch_browser will not succeed."},
-  # TODO: This error is due to not handling `:amoc_dist` yet. It should be removed when we do.
-  {"lib/live_load.ex", "The pattern can never match the type :ok | {:error, _}."}
+  {"lib/live_load/browser/connection/playwright.ex", "The function call launch_browser will not succeed."}
 ]
