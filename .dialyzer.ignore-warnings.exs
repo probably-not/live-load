9d20e58e316115a711068b6f4efbf04dddee5084
Coby Benveniste <coby.benveniste@gmail.com>
2026-03-20 10:00:59 +0200

    Remove no longer needed dialyzer supression for incorrect error spec from amoc 4.0

--- .dialyzer.ignore-warnings.exs
+++ .dialyzer.ignore-warnings.exs
@@ -5,8 +5,6 @@
   # Maybe we can fix it at some point...
   {"lib/live_load/browser/connection/playwright.ex", "Function after_start/1 has no local return."},
   {"lib/live_load/browser/connection/playwright.ex", "The function call launch_browser will not succeed."},
-  # TODO: This error is due to a mismatched spec and implementation in AMoC. It needs to be fixed in the upstream.
-  {"lib/live_load.ex", "The pattern can never match the type {:error, _}."},
   # TODO: This error is due to not handling `:amoc_dist` yet. It should be removed when we do.
   {"lib/live_load.ex", "The pattern can never match the type :ok | {:error, _}."}
 ]
