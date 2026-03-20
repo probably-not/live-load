daf5bec58f14f0210cce72b1e662f03d7ffe8a2f
Coby Benveniste <coby.benveniste@gmail.com>
2026-03-20 14:24:48 +0200

    Implement a node local listener and a master node collector for aggregating telemetry

---
+++ lib/live_load/telemetry/collector.ex
@@ -0,0 +1,75 @@
+defmodule LiveLoad.Telemetry.Collector do
+  @moduledoc false
+  # `LiveLoad.Telemetry.Collector` runs on the controller node that is running the load test.
+  # Each `LiveLoad.Telemetry.Listener` sends its completed report of telemetry received on the node
+  # to the `LiveLoad.Telemetry.Collector`, which merges all of the telemetry data into a single benchmark report.
+  # The `LiveLoad.Telemetry.Collector` determines when all nodes have completed running their load test, and
+  # signals the controller process that the load test has completed.
+
+  use GenServer
+
+  require Logger
+
+  def start_link(expected_nodes) when is_list(expected_nodes) do
+    GenServer.start_link(__MODULE__, expected_nodes)
+  end
+
+  def wait_for_completion(server, timeout \\ :infinity) do
+    results = GenServer.call(server, :wait_for_completion, timeout)
+    {:ok, results}
+  catch
+    :exit, reason ->
+      Logger.warning([
+        "[LiveLoad.Telemetry.Collector] exit when waiting for completion:",
+        " ",
+        Exception.format_exit(reason)
+      ])
+
+      {:error, reason}
+  end
+
+  def node_complete(server, stats) do
+    GenServer.cast(server, {:node_complete, node(), stats})
+  end
+
+  defmodule State do
+    @moduledoc false
+    @type t() :: %__MODULE__{
+            expected_nodes: MapSet.t(node()),
+            waiters: [GenServer.from()],
+            node_stats: %{optional(node()) => %{}}
+          }
+
+    defstruct [:expected_nodes, waiters: [], node_stats: %{}]
+
+    def new(expected_nodes) when is_list(expected_nodes) do
+      %__MODULE__{expected_nodes: MapSet.new(expected_nodes)}
+    end
+  end
+
+  @impl true
+  def init(expected_nodes) do
+    {:ok, State.new(expected_nodes)}
+  end
+
+  @impl true
+  def handle_call(:wait_for_completion, from, %State{} = state) do
+    if map_size(state.node_stats) >= MapSet.size(state.expected_nodes) do
+      {:reply, state.node_stats, state}
+    else
+      {:noreply, %{state | waiters: [from | state.waiters]}}
+    end
+  end
+
+  @impl true
+  def handle_cast({:node_complete, node, stats}, %State{} = state) do
+    state = %{state | node_stats: Map.put(state.node_stats, node, stats)}
+    {:noreply, tap(state, &maybe_notify_waiters/1)}
+  end
+
+  defp maybe_notify_waiters(%State{} = state) do
+    if map_size(state.node_stats) >= MapSet.size(state.expected_nodes) do
+      Enum.each(state.waiters, &GenServer.reply(&1, state.node_stats))
+    end
+  end
+end
---
+++ lib/live_load/telemetry/listener.ex
@@ -0,0 +1,113 @@
+defmodule LiveLoad.Telemetry.Listener do
+  @moduledoc false
+  # `LiveLoad.Telemetry.Listener` is the node-local telemetry handler module running
+  # on each of the nodes running the load test. Since LiveLoad runs distributed on all
+  # nodes in the node pools available, we need to listen to telemetry and metrics on each
+  # node and then forward the telemetry to the controller node.
+
+  use GenServer
+
+  require Logger
+
+  def start_link(collector_pid) when is_pid(collector_pid) do
+    case GenServer.start_link(__MODULE__, collector_pid) do
+      {:ok, pid} when is_pid(pid) -> {:ok, tap(pid, &install/1)}
+      # For right now, the listener doesn't have a name, so I'm not adding a catch
+      # for `{:error, {:already_started, pid}}`. Since this runs on each child node
+      # for the duration of the load test, I don't think it should be named.
+      other -> other
+    end
+  end
+
+  def handle_telemetry(event, measurements, metadata, %{listener: server}) do
+    GenServer.cast(server, {:telemetry, event, measurements, metadata})
+  end
+
+  defp install(server) do
+    handlers = %{
+      [:amoc, :scenario, :start, :start] => &__MODULE__.handle_telemetry/4,
+      [:amoc, :scenario, :start, :stop] => &__MODULE__.handle_telemetry/4,
+      [:amoc, :scenario, :start, :exception] => &__MODULE__.handle_telemetry/4
+    }
+
+    for {key, fun} <- handlers do
+      :telemetry.attach({__MODULE__, key}, key, fun, %{listener: server})
+    end
+
+    :ok
+  end
+
+  defmodule State do
+    @moduledoc false
+
+    @type t() :: %__MODULE__{
+            collector_pid: pid(),
+            started: MapSet.t(:amoc_scenario.user_id()),
+            stopped: MapSet.t(:amoc_scenario.user_id()),
+            succeeded: non_neg_integer(),
+            failed: non_neg_integer()
+          }
+
+    defstruct [
+      :collector_pid,
+      started: MapSet.new(),
+      stopped: MapSet.new(),
+      succeeded: 0,
+      failed: 0
+    ]
+
+    def new(collector_pid) do
+      %__MODULE__{collector_pid: collector_pid}
+    end
+  end
+
+  @impl true
+  def init(collector_pid) do
+    {:ok, State.new(collector_pid)}
+  end
+
+  @impl true
+  def handle_cast({:telemetry, [:amoc, :scenario, :start, :start], _measurements, %{user_id: user_id}}, %State{} = state) do
+    {:noreply, %{state | started: MapSet.put(state.started, user_id)}}
+  end
+
+  def handle_cast({:telemetry, [:amoc, :scenario, :start, :stop], _measurements, %{user_id: user_id}}, %State{} = state) do
+    state = %{state | stopped: MapSet.put(state.stopped, user_id), succeeded: state.succeeded + 1}
+    {:noreply, tap(state, &maybe_send_completion/1)}
+  end
+
+  def handle_cast(
+        {:telemetry, [:amoc, :scenario, :start, :exception], _measurements, %{user_id: user_id}},
+        %State{} = state
+      ) do
+    state = %{state | stopped: MapSet.put(state.stopped, user_id), failed: state.failed + 1}
+    {:noreply, tap(state, &maybe_send_completion/1)}
+  end
+
+  @impl true
+  def handle_cast({:telemetry, event, measurements, metadata}, %State{} = state) do
+    Logger.warning([
+      "[LiveLoad.Telemetry.Listener] Unhandled telemetry event received:",
+      " ",
+      inspect(event),
+      "; ",
+      inspect(measurements),
+      "; ",
+      inspect(metadata)
+    ])
+
+    {:noreply, state}
+  end
+
+  defp maybe_send_completion(%State{} = state) do
+    if MapSet.size(state.started) > 0 and MapSet.equal?(state.started, state.stopped) do
+      stats = %{
+        total: MapSet.size(state.stopped),
+        succeeded: state.succeeded,
+        failed: state.failed
+      }
+
+      :ok = LiveLoad.Telemetry.Collector.node_complete(state.collector_pid, stats)
+    end
+  end
+end
