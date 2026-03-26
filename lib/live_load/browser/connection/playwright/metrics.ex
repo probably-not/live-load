5a62d078d0942e8bfd526c80f91c8d878ff7a86f
Coby Benveniste <coby.benveniste@gmail.com>
2026-03-26 15:31:25 +0200

    Telemetry galore

--- lib/live_load/browser/connection/playwright/metrics.ex
+++ lib/live_load/browser/connection/playwright/metrics.ex
@@ -15,37 +15,153 @@ defmodule LiveLoad.Browser.Connection.Playwright.Metrics do
     GenServer.call(server, {:watch_page, guid, timeout})
   end
 
-  @impl true
-  def init(connection) do
-    {:ok, {connection, File.open!("./event_log.jsonl", [:read, :write, :binary])}}
+  defmodule State do
+    @moduledoc false
+
+    @type t() :: %__MODULE__{
+            connection: GenServer.name(),
+            subscribed_websockets: %{String.t() => String.t()}
+          }
+
+    defstruct [:connection, subscribed_websockets: %{}]
+
+    def new(connection) do
+      %__MODULE__{connection: connection}
+    end
   end
 
   @impl true
-  def handle_call({:watch_page, guid, timeout}, _from, {connection, outfile}) do
-    :ok = PlaywrightEx.subscribe(guid, connection: connection)
+  def init(connection) do
+    {:ok, State.new(connection)}
+  end
+
+  @impl true
+  def handle_call({:watch_page, guid, timeout}, _from, %State{} = state) do
+    :ok = PlaywrightEx.subscribe(guid, connection: state.connection)
 
     Enum.each([:console, :request, :response, :requestFinished, :requestFailed], fn event ->
       {:ok, _} =
-        PlaywrightEx.Page.update_subscription(guid, event: event, enabled: true, timeout: timeout, connection: connection)
+        PlaywrightEx.Page.update_subscription(guid,
+          event: event,
+          enabled: true,
+          timeout: timeout,
+          connection: state.connection
+        )
     end)
 
-    {:reply, :ok, {connection, outfile}}
+    {:reply, :ok, state}
+  end
+
+  ###################################
+  ## Request related subscriptions
+  ###################################
+
+  @impl true
+  def handle_info({:playwright_msg, %{method: :__create__, params: %{type: "Request"} = params}}, %State{} = state) do
+    :ok = PlaywrightEx.subscribe(params.guid, connection: state.connection)
+
+    :telemetry.execute(
+      [:live_load, :http, :request, :start],
+      %{system_time: System.system_time(), monotonic_time: System.monotonic_time()},
+      %{
+        url: params.initializer.url,
+        resource_type: params.initializer.resource_type,
+        method: params.initializer.method,
+        is_navigation: params.initializer.is_navigation_request,
+        span_id: params.guid
+      }
+    )
+
+    {:noreply, state}
   end
 
   @impl true
-  def handle_info({:playwright_msg, %{params: %{type: "Request"} = params} = _message}, {connection, outfile}) do
-    :ok = PlaywrightEx.subscribe(params.guid, connection: connection)
-    {:noreply, {connection, outfile}}
+  def handle_info({:playwright_msg, %{method: :__create__, params: %{type: "Response"} = params}}, %State{} = state) do
+    timing = params.initializer.timing
+
+    content_length =
+      Enum.find_value(params.initializer.headers, fn
+        %{name: "content-length", value: value} -> String.to_integer(value)
+        _ -> nil
+      end)
+
+    :telemetry.execute(
+      [:live_load, :http, :request, :stop],
+      %{
+        monotonic_time: System.monotonic_time(),
+        duration: timing_to_native(timing.response_start),
+        dns: timing_to_native(timing.domain_lookup_end - timing.domain_lookup_start),
+        connect: timing_to_native(timing.connect_end - timing.connect_start),
+        tls: timing_to_native(timing.connect_end - timing.secure_connection_start),
+        ttfb: timing_to_native(timing.response_start - timing.request_start),
+        content_length: content_length
+      },
+      %{url: params.initializer.url, status: params.initializer.status, span_id: params.initializer.request.guid}
+    )
+
+    {:noreply, state}
+  end
+
+  ###################################
+  ## Websocket related subsriptions
+  ###################################
+
+  @impl true
+  def handle_info({:playwright_msg, %{method: :__create__, params: %{type: "WebSocket"} = params}}, %State{} = state) do
+    :ok = PlaywrightEx.subscribe(params.guid, connection: state.connection)
+    :telemetry.execute([:live_load, :websocket, :opened], %{count: 1}, %{url: params.initializer.url})
+    new = Map.put(state.subscribed_websockets, params.guid, params.initializer.url)
+    {:noreply, %{state | subscribed_websockets: new}}
   end
 
   @impl true
-  def handle_info({:playwright_msg, %{params: %{type: "WebSocket"} = params} = _message}, {connection, outfile}) do
-    :ok = PlaywrightEx.subscribe(params.guid, connection: connection)
-    {:noreply, {connection, outfile}}
+  def handle_info({:playwright_msg, %{method: :close, guid: guid}}, %State{} = state)
+      when is_map_key(state.subscribed_websockets, guid) do
+    {url, left} = Map.pop!(state.subscribed_websockets, guid)
+    :telemetry.execute([:live_load, :websocket, :closed], %{count: 1}, %{url: url})
+    {:noreply, %{state | subscribed_websockets: left}}
   end
 
   @impl true
-  def handle_info({:playwright_msg, _message}, {connection, outfile}) do
-    {:noreply, {connection, outfile}}
+  def handle_info(
+        {:playwright_msg, %{method: :frame_sent, guid: guid, params: %{data: data, opcode: opcode}}},
+        %State{} = state
+      )
+      when is_map_key(state.subscribed_websockets, guid) do
+    :telemetry.execute(
+      [:live_load, :websocket, :frame_sent],
+      %{payload_size: byte_size(data)},
+      %{url: Map.fetch!(state.subscribed_websockets, guid), opcode: opcode}
+    )
+
+    {:noreply, state}
   end
+
+  @impl true
+  def handle_info(
+        {:playwright_msg, %{method: :frame_received, guid: guid, params: %{data: data, opcode: opcode}}},
+        %State{} = state
+      )
+      when is_map_key(state.subscribed_websockets, guid) do
+    :telemetry.execute(
+      [:live_load, :websocket, :frame_received],
+      %{payload_size: byte_size(data)},
+      %{url: Map.fetch!(state.subscribed_websockets, guid), opcode: opcode}
+    )
+
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_info({:playwright_msg, message}, %State{} = state) do
+    # TODO: I need to add a debug listener that we can attach that writes to a file later on.
+    # From some basic experimentation (me in IEx) it looks like the only things that are getting caught
+    # here are the "leftovers", along with "Worker" and "Frame" messages. I think that's stuff that I can
+    # leave off... but for debugging purposes, firing this event can help find other things in the future.
+    :telemetry.execute([:live_load, :unknown_playwright_message], %{count: 1}, %{message: message})
+    {:noreply, state}
+  end
+
+  defp timing_to_native(timing) when timing < 0, do: -1
+  defp timing_to_native(timing), do: System.convert_time_unit(round(timing * 1000), :microsecond, :native)
 end
