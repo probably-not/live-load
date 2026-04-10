05f6d878c81985c18ea263ad3fadb4c5f063c00e
Coby Benveniste <coby.benveniste@gmail.com>
2026-04-10 10:49:04 +0300

    Update dialyzer ignores

--- .dialyzer.ignore-warnings.exs
+++ .dialyzer.ignore-warnings.exs
@@ -1,6 +1,7 @@
 [
   {"lib/mix/tasks/live_load/install.ex", "Callback info about the Mix.Task behaviour is not available."},
   {"lib/mix/tasks/live_load/install.ex", "Function Mix.shell/0 does not exist."},
+  {"lib/mix/tasks/live_load/install.ex", "Function Mix.raise/1 does not exist."},
   {"lib/live_load/scenario/discovery.ex", "Function Mix.Project.get/0 does not exist."},
   {"lib/live_load/scenario/discovery.ex", "Function Mix.Project.config/0 does not exist."}
 ]
